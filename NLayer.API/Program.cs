c8463b97c10e0ad0bcc2809c26a78977ab0eabc3
Ahmet Emre Karaca <78788788+Eolan-Sama@users.noreply.github.com>
2022-12-23 14:17:59 +0300

    Add files via upload

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,9 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.UnitOfWorks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
 
 var app = builder.Build();
 
